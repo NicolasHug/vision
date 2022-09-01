fix iterate_one_epoch to avoid memory overflow and empty datasets

benchmark_data_loading/common.py
@@ -62,9 +62,14 @@ def bench(f, inp, num_exp=3, warmup=1, unit="μ", num_images_per_call=DATASET_SI
 
 def iterate_one_epoch(obj):
     if isinstance(obj, (data.datapipes.datapipe.IterDataPipe, FFCVLoader, data.DataLoader, DataLoader2)):
+        i = 0
         for _ in obj:
-            pass
+            i += 1
+        if i == 0:
+            raise RuntimeError("Dataset contains no samples!")
     elif isinstance(obj, ImageFolder):
+        if len(obj) == 0:
+            raise RuntimeError("Dataset contains no samples!")
         # Need to reproduce "random" access
         indices = torch.randperm(len(obj))
         for i in indices: